98a10570388bc03631618fc6e81c902eb7c3347f
Julian Ehiem <jules.ehiem@gmail.com>
2021-05-06 10:51:39 -0400

    updated nav layout

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,18 +3,26 @@ import navstyles from './navigation.module.css'
 const Navigation = () => {
     return(
         <div className={navstyles.navbar}>
-            <ul className={navstyles.navbarMenu}>
-                <li>Search</li>
-                <li>Menu</li>
-                <li>Dashboard</li>
-                <li>Training</li>
-                <li>Explore</li>
-                <li>Challenges</li>
-                <li>Button</li>
-                <li>Bell</li>
-                <li>Profile</li>
-                <li>New Activity</li>
-            </ul>
+            <div className={navstyles.navContainer}>
+                <div className={navstyles.navLeft}>
+                    <ul className={navstyles.navbarMenu}>
+                        <li><a href = "#" >Search</a></li>
+                        <li><a href = "#" >Menu</a></li>
+                        <li><a href = "#" >Dashboard</a></li>
+                        <li><a href = "#" >Training</a></li>
+                        <li><a href = "#" >Explore</a></li>
+                        <li><a href = "#" >Challenges</a></li>
+                    </ul>
+                </div>
+                <div className={navstyles.navRight}>
+                    <ul className={navstyles.navbarMenu}>
+                        <li><a href = "#" >Button</a></li>
+                        <li><a href = "#" >Bell</a></li>
+                        <li><a href = "#" >Profile</a></li>
+                        <li><a href = "#" >New Activity</a></li>
+                    </ul>
+                </div>
+            </div> 
         </div>
     )
 }
