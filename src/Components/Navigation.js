c9512a3211535d82f8852eb28b86976d243a51ab
Julian Ehiem <jules.ehiem@gmail.com>
2021-05-06 12:46:28 -0400

    adjusted arrow sizes

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,9 @@ import navstyles from './navigation.module.css'
 import like from '../Icons/search-icon.svg'
 import hamburger from '../Icons/hamburger.svg'
 import arrow from '../Icons/arrow-down.svg'
+import bell from '../Icons/bell.svg'
+import profileImage from '../Icons/profileImage.svg'
+import activityIcon from '../Icons/activityIcon.svg'
 
 const Navigation = () => {
     return(
@@ -10,21 +13,20 @@ const Navigation = () => {
                 <div className={navstyles.navLeft}>
                     <ul className={navstyles.navbarMenu}>
                         <li><a href = "#" className={navstyles.navLogo}>STREBEN</a></li>
-                        <li><a href = "#" ><img src={like} alt="search icon"/></a></li>
-                        <li><a href = "#" > <span className={navstyles.hamburgerSet}><img src={hamburger} alt="hamburger menu icon"/><img src={arrow} alt="arrow icon"/></span></a></li>
-                        {/* <li><a href = "#" >Menu</a></li> */}
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Dashboard <img src={arrow} alt="arrow icon"/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Training <img src={arrow} alt="arrow icon"/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Explore <img src={arrow} alt="arrow icon"/></span></a></li>
-                        <li><a href = "#" >Challenges</a></li>
+                        <li><a href = "#" ><img src={like} alt="search icon" width={20}/></a></li>
+                        <li><a href = "#" > <span className={navstyles.hamburgerSet}><img src={hamburger} alt="hamburger menu icon" width={20}/><img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Dashboard <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Training <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Explore <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" > <span className={navstyles.dropdownSet}>Challenges</span></a></li>
                     </ul>
                 </div>
                 <div className={navstyles.navRight}>
                     <ul className={navstyles.navbarMenu}>
                         <li><button href = "#" className={navstyles.navBtn}>Give a Subscription</button></li>
-                        <li><a href = "#" >Bell</a></li>
-                        <li><a href = "#" >Profile</a></li>
-                        <li><a href = "#" >New Activity</a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}><img src={bell} alt="bell icon"/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}><img src={profileImage} alt="profile icon"/><img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}><img src={activityIcon} alt="activity icon"/></span></a></li>
                     </ul>
                 </div>
             </div> 
