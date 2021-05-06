7a919355dc5eaf4ace8821572e4fcfce40cb2d9f
Julian Ehiem <jules.ehiem@gmail.com>
2021-05-06 14:15:49 -0400

    finished initial navigation layout

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -13,20 +13,20 @@ const Navigation = () => {
                 <div className={navstyles.navLeft}>
                     <ul className={navstyles.navbarMenu}>
                         <li><a href = "#" className={navstyles.navLogo}>STREBEN</a></li>
-                        <li><a href = "#" ><img src={like} alt="search icon" width={20}/></a></li>
-                        <li><a href = "#" > <span className={navstyles.hamburgerSet}><img src={hamburger} alt="hamburger menu icon" width={20}/><img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Dashboard <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Training <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Explore <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
-                        <li><a href = "#" > <span className={navstyles.dropdownSet}>Challenges</span></a></li>
+                        <li><a href = "#" ><img src={like} alt="search icon" width={17}/></a></li>
+                        <li><a href = "#" ><span><img src={hamburger} alt="hamburger menu icon" width={20} className={navstyles.hamburger}/><img src={arrow} alt="arrow icon" className={`${navstyles.arrowSize} ${navstyles.hamArrow}`}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}>Dashboard <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}>Training <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}>Explore <img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}>Challenges</span></a></li>
                     </ul>
                 </div>
                 <div className={navstyles.navRight}>
                     <ul className={navstyles.navbarMenu}>
                         <li><button href = "#" className={navstyles.navBtn}>Give a Subscription</button></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}><img src={bell} alt="bell icon"/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}><img src={profileImage} alt="profile icon"/><img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
-                        <li><a href = "#" ><span className={navstyles.dropdownSet}><img src={activityIcon} alt="activity icon"/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}><img src={bell} alt="bell icon"/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}><img src={profileImage} alt="profile icon"/><img src={arrow} alt="arrow icon" className={navstyles.arrowSize}/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.drop}><img src={activityIcon} alt="activity icon"/></span></a></li>
                     </ul>
                 </div>
             </div> 
