f9d4687eaf13e35e9a270a31bebed5ad24105598
Julian Ehiem <jules.ehiem@gmail.com>
2021-05-06 11:49:03 -0400

    added subscription button

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,4 +1,7 @@
 import navstyles from './navigation.module.css'
+import like from '../Icons/search-icon.svg'
+import hamburger from '../Icons/hamburger.svg'
+import arrow from '../Icons/arrow-down.svg'
 
 const Navigation = () => {
     return(
@@ -7,18 +10,18 @@ const Navigation = () => {
                 <div className={navstyles.navLeft}>
                     <ul className={navstyles.navbarMenu}>
                         <li><a href = "#" className={navstyles.navLogo}>STREBEN</a></li>
-                        {/* <li><a href = "#" className={navstyles.navbarMenu}><span >STREBEN</span></a></li> */}
-                        <li><a href = "#" >Search</a></li>
-                        <li><a href = "#" >Menu</a></li>
-                        <li><a href = "#" >Dashboard</a></li>
-                        <li><a href = "#" >Training</a></li>
-                        <li><a href = "#" >Explore</a></li>
+                        <li><a href = "#" ><img src={like} alt="search icon"/></a></li>
+                        <li><a href = "#" > <span className={navstyles.hamburgerSet}><img src={hamburger} alt="hamburger menu icon"/><img src={arrow} alt="arrow icon"/></span></a></li>
+                        {/* <li><a href = "#" >Menu</a></li> */}
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Dashboard <img src={arrow} alt="arrow icon"/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Training <img src={arrow} alt="arrow icon"/></span></a></li>
+                        <li><a href = "#" ><span className={navstyles.dropdownSet}>Explore <img src={arrow} alt="arrow icon"/></span></a></li>
                         <li><a href = "#" >Challenges</a></li>
                     </ul>
                 </div>
                 <div className={navstyles.navRight}>
                     <ul className={navstyles.navbarMenu}>
-                        <li><a href = "#" >Button</a></li>
+                        <li><button href = "#" className={navstyles.navBtn}>Give a Subscription</button></li>
                         <li><a href = "#" >Bell</a></li>
                         <li><a href = "#" >Profile</a></li>
                         <li><a href = "#" >New Activity</a></li>
