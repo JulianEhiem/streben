c33760c283d09d7ecde8fffd4b3fb3b350c96e55
Julian Ehiem <jules.ehiem@gmail.com>
2021-05-07 16:41:34 -0400

    added svg components for goals card

--- src/Pages/Dashboard/LeftSideBarComponents/AthleteGoalsDashboard.js
+++ src/Pages/Dashboard/LeftSideBarComponents/AthleteGoalsDashboard.js
@@ -1,10 +1,34 @@
-import dashstyles from '../dashboard.module.css'
+import leftstyles from './leftComponents.module.css'
 
 
 const AthleteGoalsDashboard = () => {
     return(
-        <div className={dashstyles.test2}>
-            This is the Goal Dashboard
+        <div className={leftstyles.goalsDashContainer}>
+            <div className={leftstyles.goalsDashMain}>
+                <div className={leftstyles.activityTabs}>
+                    <div className={leftstyles.relativeEffortTab}></div>
+                    <div className={leftstyles.runningTab}></div>
+                    <div className={leftstyles.bikingTab}></div>
+                    <div className={leftstyles.swimmingTab}></div>
+                </div> 
+                <div className={leftstyles.mileageReport}>
+                    <div className={leftstyles.thisWeek}></div>
+                    <div className={leftstyles.edit}></div>
+                    <div className={leftstyles.currentMileage}></div>
+                    <div className={leftstyles.dailyStats}></div>
+                    <div className={leftstyles.weeklyPie}></div>
+                    <div className={leftstyles.hoursAndElevation}></div>
+                </div> 
+                <div className={leftstyles.yearlyProgress}>
+                    <div className={leftstyles.thisYear}></div>
+                    <div className={leftstyles.yearlyMileage}></div>
+                    <div className={leftstyles.mileageProgressBar}></div>
+                    <div className={leftstyles.mileageStatus}></div>
+                </div> 
+                <div className={leftstyles.manageGoals}></div> 
+            </div>
+            
+            Goals Dash
         </div>
     )
 }
